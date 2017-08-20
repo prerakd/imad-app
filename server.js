957ad294d3156fb8bb4d8ac919fc50f6e16e2a0a
Prerak Dholakiya <prerak.dholakia@gmail.com>
2017-08-20 17:31:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
+var Pool = require('pg').Pool;
+
+var config = {
+    user: 'prerakdholakia',
+    database: 'prerakdholakia',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+}
+
 var app = express();
 app.use(morgan('combined'));
 
@@ -16,6 +26,19 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+
+var pool = new Pool(config);
+app.get('/test-db', function (req, res){
+    pool.query('SELCT * from test', function (err, result){
+        if(err){
+            res.status(500).send(err.toString());
+        } else {
+            res.send(JSON.stringify(result));
+        }
+        
+    })
+    
+})
 
 
 // Do not change port, otherwise your app won't run on IMAD servers
