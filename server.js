043ffa4aef556a4a02e52e2eaf72d1785424536d
Prerak Dholakiya <prerak.dholakia@gmail.com>
2017-08-25 00:44:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.post('/create-user', function(req, res){
-    var username = req.body.usrename;
+    var username = req.body.username;
     var password = req.body.password;
     var salt = crypto.randomBytes(128).toString('hex');
     var dbString = hash(password, salt);
