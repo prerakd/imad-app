25773dc0b91a84409e324165a489b8e504fcd285
Prerak Dholakiya <prerak.dholakia@gmail.com>
2017-08-25 00:55:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.post('/create-user', function(req, res){
     var password = req.body.password;
     var salt = crypto.randomBytes(128).toString('hex');
     var dbString = hash(password, salt);
-    pool.query('INSERT INTO "user" (userName, password) VALUES $1 $2', [username, password], function(err, res){
+    pool.query('INSERT INTO "user" (userName, password) VALUES $1 $2', [username, password], function(err, result){
         if(err){
             res.status(500).send(err.toString());
         } else {
