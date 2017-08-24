c85364db307151fa61f1afd2ddb4a9674d556581
Prerak Dholakiya <prerak.dholakia@gmail.com>
2017-08-25 01:31:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,23 @@
-console.log('Loaded!');
+var submit = document.getElementById('submit_btn');
+submit.onClick = function(){
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status === 200){
+                console.log('user logged in ');
+                alert('Logged in successfully');
+            } else if(request.status === 403){
+                 alert('username/password is incorrect');
+            }
+            else if(request.status === 500){
+                alert('c skvjvn');
+            }
+        }
+    }
+    
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    request.open('POST', 'http://prerakdholakia.imad.hasura-app.io/login', true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({ username: username, password: password}));
+}
