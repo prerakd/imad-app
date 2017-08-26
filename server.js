cbe3793fba049e6cb17af07a6a140f345cd3d809
Prerak Dholakiya <prerak.dholakia@gmail.com>
2017-08-26 12:09:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ app.get('/ui/main.js', function (req, res) {
 });
 
 app.get('/check-login', function(req, res){
-    if(req.session && req.session.auth && !req.session.auth.userId){
+    if(req.session && !req.session.auth && !req.session.auth.userId){
           res.send("You are logged in", req.session.auth.userId.toString());
     } else {
         res.send("You are not logged in ");
